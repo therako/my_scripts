ba098d98f57c93d8adecca43817c626dfe4f7ddb
therako <rako.aka.arun@gmail.com>
2017-08-13 14:24:03 -0300

    Updating alertmanager setup script.

--- bash/prometheus/alertmanager/startup.sh
+++ bash/prometheus/alertmanager/startup.sh
@@ -12,6 +12,49 @@ if [ ! -f "${EXECUTABLE}" ]; then
     tar -xf /tmp/alertmanager-0.8.0.linux-amd64.tar.gz --directory /opt/
 fi
 
+# Alermanager config
+cat >${ROOT_DIR}/templates/node.tmpl <<EOF
+{{ define "slack.node.title" }}[{{ .Status | toUpper }}{{ if eq .Status "firing" }}: {{ .Alerts.Firing | len }}{{ end }}] {{ .GroupLabels.alertname }} on {{ .GroupLabels.cluster }}{{ end }}
+
+{{ define "slack.node.instances" }}{{ range . }}- *{{ .Labels.host }}* ({{ .Labels.instance }})
+{{ end }}{{ end }}
+
+{{ define "slack.node.text" }}
+{{ .CommonAnnotations.summary }}.
+
+{{ if gt (len .Alerts.Firing) 0 -}}Alerts Firing:
+{{ template "slack.node.instances" .Alerts.Firing }}{{- end }}{{ if gt (len .Alerts.Resolved) 0 -}}Alerts Resolved:
+{{ template "slack.node.instances" .Alerts.Resolved }}{{ end }}
+
+Grafana: http://localhost:3000/dashboard/db/node?var-cluster={{ .GroupLabels.cluster }}
+{{ end }}
+EOF
+
+cat >${ROOT_DIR}/alertmanager.yml <<EOF
+global:
+
+route:
+  receiver: 'slack'
+  group_by: ['alertname', 'cluster']
+  group_wait: 3s
+  group_interval: 3s
+  repeat_interval: 30m
+  routes:
+  - receiver: 'slack'
+
+receivers:
+- name: 'slack'
+  slack_configs:
+  - api_url: 'Slack webbhook url'
+    channel: '#channel-name'
+    title: '{{ template "slack.node.title" . }}'
+    text: '{{ template "slack.node.text" . }}'
+    send_resolved: True
+
+templates:
+  - ${ROOT_DIR}/templates/node.tmpl
+EOF
+
 
 # setup systemctl script for alertmanager
 cat >/lib/systemd/system/alertmanager.service <<EOF
