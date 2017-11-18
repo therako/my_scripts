e091a7f2cfb9ba3d04a1ba060a767ec33e44f204
AK <ak@thecarousell.com>
2017-11-18 12:26:38 +0800

    Docker clean up script.

---
+++ bash/docker/cleanup_old_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "remove exited containers"
+docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
+echo "remove unused images"
+docker images --no-trunc | grep 'days' | awk '{ print $3 }' | xargs docker rmi
