1af7a7417294e36932d9e9d83a5499710dd7f8b5
Arun Kumar (AK) <ak@thecarousell.com>
2018-05-09 18:08:08 +0800

    Updating docker clean up script
    Making it clean any image that's older than 1 day

--- bash/docker/cleanup_old_images.sh
+++ bash/docker/cleanup_old_images.sh
@@ -3,4 +3,4 @@
 echo "remove exited containers"
 docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
 echo "remove unused images"
-docker images --no-trunc | grep 'days' | awk '{ print $3 }' | xargs docker rmi
+docker images --no-trunc | grep -v 'hour' | awk '{ print $3 }' | xargs docker rmi
